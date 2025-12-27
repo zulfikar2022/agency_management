e8e6f910cef4193753f4d76cbee1c38b24e861c0
Sayed Zulfikar Mahmud <sayedzulfikar2019@gmail.com>
2025-12-27 15:38:19 +0600

    feat: Enhance user restoration functionality in Super Admin dashboard with confirmation dialog

--- resources/js/Pages/SuperAdmin/SuperAdminAllMembers.jsx
+++ resources/js/Pages/SuperAdmin/SuperAdminAllMembers.jsx
@@ -6,9 +6,13 @@ import { Bounce, toast } from 'react-toastify';
 
 // superadmin.toggleDeleteStatus
 function SuperAdminAllMembers({ users }) {
-  const deleteUser = (userId) => {
+  console.log(users[0]);
+  const deleteUser = (user) => {
     Swal.fire({
-      text: 'Are you sure you want to delete this user with id ' + userId + '?',
+      text:
+        'Are you sure you want to delete this user with name ' +
+        user.name +
+        '?',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -16,7 +20,7 @@ function SuperAdminAllMembers({ users }) {
       confirmButtonText: 'Yes, delete it!',
     }).then((result) => {
       if (result.isConfirmed) {
-        router.post(route('superadmin.toggleDeleteStatus', { user: userId }), {
+        router.post(route('superadmin.toggleDeleteStatus', { user: user.id }), {
           onSuccess: () => {
             toast.success('User deleted successfully!', {
               position: 'top-center',
@@ -36,83 +40,89 @@ function SuperAdminAllMembers({ users }) {
   };
   return (
     <SuperAdminDashboard>
-      <h1 className="font-bold text-2xl text-center my-4">
-        All (Not Deleted) Members
-      </h1>
-      {users?.length === 0 && <p>No members found.</p>}
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className={`p-4 mb-4 border rounded shadow mx-5 md:mx-0 ${user.is_activated ? '' : 'bg-red-100'} ${user.is_super_admin ? 'bg-green-300' : ''}`}
-        >
-          <h2 className="text-xl font-bold">{user.name}</h2>
-          <p className="font-bold">
-            Email: <span className="font-normal">{user.email}</span>
-          </p>
-          <p className="font-bold">
-            User id : <span className="font-normal">{user.id}</span>
-          </p>
-          <div className="flex gap-2">
+      <div className=" ">
+        <h1 className="font-bold text-2xl text-center my-4">
+          All (Not Deleted) Members
+        </h1>
+        {users?.length === 0 && (
+          <p className="text-center">No members found.</p>
+        )}
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className={`p-4 mb-4 border rounded shadow mx-5 md:mx-0 ${user.is_activated ? '' : 'bg-red-100'} ${user.is_super_admin ? 'bg-green-300' : ''}`}
+          >
+            <h2 className="text-xl font-bold">{user.name}</h2>
             <p className="font-bold">
-              is_super_admin:{' '}
-              <span className="font-normal">
-                {user.is_super_admin ? 'Yes' : 'No'}
-              </span>
+              Email: <span className="font-normal">{user.email}</span>
             </p>
-          </div>
-          <div className="flex gap-2">
             <p className="font-bold">
-              is_admin:{' '}
-              <span className="font-normal">
-                {user.is_admin ? 'Yes' : 'No'}
-              </span>
+              User id : <span className="font-normal">{user.id}</span>
             </p>
-            {!user.is_super_admin && (
-              <p className=" rounded-lg cursor-pointer">
-                <ToggleLeft />
+            {user.is_super_admin ? (
+              <div className="flex gap-2">
+                <p className="font-bold">
+                  is_super_admin:{' '}
+                  <span className="font-normal">
+                    {user.is_super_admin ? 'Yes' : 'No'}
+                  </span>
+                </p>
+              </div>
+            ) : null}
+            <div className="flex gap-2">
+              <p className="font-bold">
+                is_admin:{' '}
+                <span className="font-normal">
+                  {user.is_admin ? 'Yes' : 'No'}
+                </span>
               </p>
-            )}
-          </div>
-          <div className="flex gap-2">
-            <p className="font-bold">
-              is_employee:{' '}
-              <span className="font-normal">
-                {' '}
-                {user.is_employee ? 'Yes' : 'No'}
-              </span>
-            </p>
-            {!user.is_super_admin && (
-              <p className=" rounded-lg cursor-pointer">
-                <ToggleLeft />
-              </p>
-            )}
-          </div>
-          <div className="flex gap-2">
-            <p className="font-bold">
-              is_activated:{' '}
-              <span className="font-normal">
-                {' '}
-                {user.is_activated ? 'Yes' : 'No'}
-              </span>
-            </p>
-            {!user.is_super_admin && (
-              <p className=" rounded-lg cursor-pointer">
-                <ToggleLeft />
-              </p>
-            )}
-          </div>
-          {!user.is_super_admin && (
-            <div>
-              <p
-                onClick={() => deleteUser(user.id)}
-                className="btn btn-error btn-xs mt-5"
-              >
-                Soft Delete
-              </p>
+              {!user.is_super_admin && (
+                <p className=" rounded-lg cursor-pointer">
+                  <ToggleLeft />
+                </p>
+              )}
             </div>
-          )}
-        </div>
-      ))}
+            <div className="flex gap-2">
+              <p className="font-bold">
+                is_employee:{' '}
+                <span className="font-normal">
+                  {' '}
+                  {user.is_employee ? 'Yes' : 'No'}
+                </span>
+              </p>
+              {!user.is_super_admin && (
+                <p className=" rounded-lg cursor-pointer">
+                  <ToggleLeft />
+                </p>
+              )}
+            </div>
+            <div className="flex gap-2">
+              <p className="font-bold">
+                is_activated:{' '}
+                <span className="font-normal">
+                  {' '}
+                  {user.is_activated ? 'Yes' : 'No'}
+                </span>
+              </p>
+              {!user.is_super_admin && (
+                <p className=" rounded-lg cursor-pointer">
+                  <ToggleLeft />
+                </p>
+              )}
+            </div>
+            {!user.is_super_admin && (
+              <div>
+                <p
+                  onClick={() => deleteUser(user)}
+                  className="btn btn-error btn-xs mt-5"
+                >
+                  Soft Delete
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </SuperAdminDashboard>
   );
 }
--- resources/js/Pages/SuperAdmin/SuperAdminDeletedMembers.jsx
+++ resources/js/Pages/SuperAdmin/SuperAdminDeletedMembers.jsx
@@ -1,11 +1,122 @@
+import { ToggleLeft } from 'lucide-react';
 import SuperAdminDashboard from './SuperAdminDashboard';
+import Swal from 'sweetalert2';
+import { Bounce, toast } from 'react-toastify';
+import { router } from '@inertiajs/react';
 
 function SuperAdminDeletedMembers({ users }) {
-  console.log(users);
+  const handleRestoreUser = (user) => {
+    Swal.fire({
+      text:
+        'Are you sure you want to restore this user with name ' +
+        user.name +
+        '?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, restore it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        router.post(route('superadmin.toggleDeleteStatus', { user: user.id }), {
+          onSuccess: () => {
+            toast.success('User restored successfully!', {
+              position: 'top-center',
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: false,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'dark',
+              transition: Bounce,
+            });
+          },
+        });
+      }
+    });
+  };
   return (
     <SuperAdminDashboard>
-      <h1 className="text-black">All Deleted Members</h1>
-      <div></div>
+      <h1 className="font-bold text-2xl text-center my-4 text-red-500">
+        All Deleted Members
+      </h1>
+      {users?.length === 0 && <p className="text-center">No members found.</p>}
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className={`p-4 mb-4 border border-red-400 rounded shadow mx-5 md:mx-0 ${user.is_activated ? '' : 'bg-red-100'} ${user.is_super_admin ? 'bg-green-300' : ''}`}
+        >
+          <h2 className="text-xl font-bold text-red-500">{user.name}</h2>
+          <p className="font-bold text-red-500">
+            Email: <span className="font-normal">{user.email}</span>
+          </p>
+          <p className="font-bold text-red-500">
+            User id : <span className="font-normal">{user.id}</span>
+          </p>
+          {user.is_super_admin ? (
+            <div className="flex gap-2">
+              <p className="font-bold">
+                is_super_admin:{' '}
+                <span className="font-normal">
+                  {user.is_super_admin ? 'Yes' : 'No'}
+                </span>
+              </p>
+            </div>
+          ) : null}
+          <div className="flex gap-2">
+            <p className="font-bold text-red-500">
+              is_admin:{' '}
+              <span className="font-normal">
+                {user.is_admin ? 'Yes' : 'No'}
+              </span>
+            </p>
+            {!user.is_super_admin && (
+              <p className=" rounded-lg cursor-pointer text-red-500">
+                <ToggleLeft />
+              </p>
+            )}
+          </div>
+          <div className="flex gap-2 ">
+            <p className="font-bold text-red-500">
+              is_employee:{' '}
+              <span className="font-normal">
+                {' '}
+                {user.is_employee ? 'Yes' : 'No'}
+              </span>
+            </p>
+            {!user.is_super_admin && (
+              <p className=" rounded-lg cursor-pointer text-red-500">
+                <ToggleLeft />
+              </p>
+            )}
+          </div>
+          <div className="flex gap-2">
+            <p className="font-bold text-red-500">
+              is_activated:{' '}
+              <span className="font-normal">
+                {' '}
+                {user.is_activated ? 'Yes' : 'No'}
+              </span>
+            </p>
+            {!user.is_super_admin && (
+              <p className=" rounded-lg cursor-pointer text-red-500">
+                <ToggleLeft />
+              </p>
+            )}
+          </div>
+          {!user.is_super_admin && (
+            <div>
+              <p
+                onClick={() => handleRestoreUser(user)}
+                className="btn btn-primary btn-xs mt-5"
+              >
+                Restore User
+              </p>
+            </div>
+          )}
+        </div>
+      ))}
     </SuperAdminDashboard>
   );
 }
